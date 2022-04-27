10c98b9c9f5278319ba048be1c5dbaf46292edea
Mirosław Gucwa <mgucwa@gmail.com>
2022-04-28 00:14:16 +0200

    Add ColumnForm

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,26 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+
+const ColumnForm = props => {
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.action({ title: title, icon: icon });
+    setTitle('');
+    setIcon('');
+  }
+
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      <label><span>Title:</span><TextInput value={title} onChange={e => setTitle(e.target.value)} /></label>
+      <label><span>Icon:</span><TextInput value={icon} onChange={e => setIcon(e.target.value)} /></label>
+      <Button>Add column</Button>
+    </form>
+  );
+};
+
+export default ColumnForm;
