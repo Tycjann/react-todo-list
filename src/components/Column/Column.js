7a42d010a62d8d0ff83aa58d431917dec3809ff6
Mirosław Gucwa <mgucwa@gmail.com>
2022-05-04 18:50:11 +0200

    Redux - load columns & cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,19 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
 
-  console.log(props.addCard);
+  const cards = useSelector(state => state.cards).filter(card => card.columnId === props.id);
+
   return (
     <article className={styles.column}>
       <h2 className={styles.title}>
         <span className={styles.icon + ' fa fa-' + props.icon} />{props.title}
       </h2>
       <ul className={styles.cards}>
-        {props.cards.map(card => 
+        {cards.map(card => 
           <Card key={card.id} title={card.title} />
         )}
         <CardForm columnId={props.id} action={props.addCard}/>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import App from './App';
@@ -7,11 +7,12 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.querySelector('#root')
+  </React.StrictMode>
 );
