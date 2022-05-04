3c40e06a69820e1dd054be50aaed8b97c4b20915
Mirosław Gucwa <mgucwa@gmail.com>
2022-05-04 17:49:31 +0200

    Instaling redux and create store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
-
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.querySelector('#root')
 );
